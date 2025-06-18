f59c71cf829b9442bbea79f3e672b53134fcd97d
tarekkkk-b <tarekbadawi2004@gmail.com>
2025-06-18 16:13:23 +0400

    tests created for module07 ex02

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 #include "Array.hpp"
 
-int main(int ac, char **av)
+int main()
 {
+	srand(time(NULL));
+	Array<std::string>	test;
+	try 
+	{
+		std::cout << test[1] << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	Array<int> testTwo(5);
+	try
+	{
+		std::cout << testTwo[1] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	try
+	{
+		for (size_t i = 0; i < testTwo.size(); i++)
+			testTwo[i] = rand();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		for (size_t i = 0; i < testTwo.size(); i++)
+			std::cout << testTwo[i] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 }
